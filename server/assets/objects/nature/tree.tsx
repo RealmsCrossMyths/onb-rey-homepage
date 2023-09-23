4eb682cfafbb79fd4536528ad0ff107b1ee2d4a6
Realms×Myths <10644454+RealmsCrossMyths@users.noreply.github.com>
2023-09-23 15:01:33 -0400

    upload latest changes

--- server/assets/objects/nature/tree.tsx
+++ server/assets/objects/nature/tree.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="Nature - Tree" tilewidth="44" tileheight="61" tilecount="4" columns="4" objectalignment="bottomleft">
- <tileoffset x="11" y="-6"/>
+<tileset version="1.8" tiledversion="1.8.0" name="Nature - Tree" tilewidth="44" tileheight="61" tilecount="4" columns="4" objectalignment="bottom">
+ <tileoffset x="1" y="2"/>
  <grid orientation="isometric" width="64" height="32"/>
  <properties>
   <property name="Solid" type="bool" value="true"/>
@@ -8,7 +8,7 @@
  <image source="tree.png" width="176" height="61"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="11" width="32" height="32">
+   <object id="1" x="1.5" y="12.5" width="29" height="29">
     <ellipse/>
    </object>
   </objectgroup>
