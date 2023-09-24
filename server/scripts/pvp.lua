cc7f9baa43a93cc5ae658eeb948b8ce4321e40fb
Realms×Myths <10644454+RealmsCrossMyths@users.noreply.github.com>
2023-09-24 17:54:31 -0400

    upload latest changes

--- server/scripts/pvp.lua
+++ server/scripts/pvp.lua
@@ -1,17 +1,25 @@
 local requests = {}
 local questioned_requests = {}
+local players_in_battle = {}
+local timer = 0
 
-function handle_player_connect(player_id)
+Net:on("player_connect", function(event)
+  local player_id = event.player_id
   requests[player_id] = {}
-end
+end)
 
-function handle_player_disconnect(player_id)
+Net:on("player_disconnect", function(event)
+  local player_id = event.player_id
   requests[player_id] = nil
   questioned_requests[player_id] = nil
-end
+end)
 
-function handle_actor_interaction(player_id, other_id, button)
+Net:on("actor_interaction", function(event)
+  local button = event.button
   if button ~= 0 then return end
+
+  local player_id = event.player_id
+  local other_id = event.actor_id
 
   if requests[other_id] == nil then
     -- other_id is not a player id, since they're not registered in the request list
@@ -22,7 +30,7 @@ function handle_actor_interaction(player_id, other_id, button)
 
   if requests[player_id][other_id] then
     -- we're responding to the other player's request
-    question = "Accept request?"
+    question = "Accept battle with\n"..Net.get_player_name(other_id).."?"
   else
     -- we're making a request for the other player
 
@@ -36,9 +44,9 @@ function handle_actor_interaction(player_id, other_id, button)
     local request_status = requests[other_id][player_id]
 
     if request_status then
-      question = "Request again?"
+      question = "Request again from\n"..Net.get_player_name(other_id).."?"
     else
-      question = "Request a battle?"
+      question = "Request PVP with\n"..Net.get_player_name(other_id).."?"
     end
   end
 
@@ -52,9 +60,11 @@ function handle_actor_interaction(player_id, other_id, button)
   )
 
   questioned_requests[player_id] = other_id
-end
+end)
 
-function handle_textbox_response(player_id, response)
+Net:on("textbox_response", function(event)
+  local response = event.response
+  local player_id = event.player_id
   if response == 0 then
     -- response was no, no action needs to be taken
     questioned_requests[player_id] = nil
@@ -72,11 +82,28 @@ function handle_textbox_response(player_id, response)
     -- we're saying yes to the other player's request
     requests[player_id][other_id] = nil
     Net.initiate_pvp(player_id, other_id)
+    players_in_battle[event.player_id] = true
   else
     -- we're making a request for the other player
     requests[other_id][player_id] = true
-    Net.exclusive_player_emote(other_id, player_id, 7) -- sword emote
+    Net.exclusive_player_emote(other_id, player_id, 10) -- question mark emote
   end
 
   questioned_requests[player_id] = nil
-end
+end)
+
+Net:on("tick", function(event)
+    timer = timer + event.delta_time
+    if timer > 20 then
+        for player_id, value in pairs(players_in_battle) do
+            Net.set_player_emote(player_id, 7) --swords emote
+        end  
+        timer = 0
+    end
+end)
+
+Net:on("battle_results", function(event)
+    if players_in_battle[event.player_id] then
+        players_in_battle[event.player_id] = nil
+    end
+end)
