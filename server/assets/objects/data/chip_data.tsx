9cd9a76ce5ba96b83104a01388cb6f7bdd53382a
Realms×Myths <10644454+RealmsCrossMyths@users.noreply.github.com>
2023-09-26 15:14:20 -0400

    upload latest changes

---
+++ server/assets/objects/data/chip_data.tsx
@@ -0,0 +1,321 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="Data - Data Chip" tilewidth="16" tileheight="40" tilecount="120" columns="24" objectalignment="center">
+ <tileoffset x="0" y="-18"/>
+ <grid orientation="isometric" width="64" height="32"/>
+ <properties>
+  <property name="Solid" type="bool" value="true"/>
+ </properties>
+ <image source="chip_data.png" width="384" height="200"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="48" duration="100"/>
+   <frame tileid="72" duration="100"/>
+   <frame tileid="96" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="49" duration="100"/>
+   <frame tileid="73" duration="100"/>
+   <frame tileid="97" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="26" duration="100"/>
+   <frame tileid="50" duration="100"/>
+   <frame tileid="74" duration="100"/>
+   <frame tileid="98" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="51" duration="100"/>
+   <frame tileid="75" duration="100"/>
+   <frame tileid="99" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="28" duration="100"/>
+   <frame tileid="52" duration="100"/>
+   <frame tileid="76" duration="100"/>
+   <frame tileid="100" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="29" duration="100"/>
+   <frame tileid="53" duration="100"/>
+   <frame tileid="77" duration="100"/>
+   <frame tileid="101" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="30" duration="100"/>
+   <frame tileid="54" duration="100"/>
+   <frame tileid="78" duration="100"/>
+   <frame tileid="102" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="31" duration="100"/>
+   <frame tileid="55" duration="100"/>
+   <frame tileid="79" duration="100"/>
+   <frame tileid="103" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="56" duration="100"/>
+   <frame tileid="80" duration="100"/>
+   <frame tileid="104" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="33" duration="100"/>
+   <frame tileid="57" duration="100"/>
+   <frame tileid="81" duration="100"/>
+   <frame tileid="105" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="34" duration="100"/>
+   <frame tileid="58" duration="100"/>
+   <frame tileid="82" duration="100"/>
+   <frame tileid="106" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="35" duration="100"/>
+   <frame tileid="59" duration="100"/>
+   <frame tileid="83" duration="100"/>
+   <frame tileid="107" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="36" duration="100"/>
+   <frame tileid="60" duration="100"/>
+   <frame tileid="84" duration="100"/>
+   <frame tileid="108" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="37" duration="100"/>
+   <frame tileid="61" duration="100"/>
+   <frame tileid="85" duration="100"/>
+   <frame tileid="109" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="38" duration="100"/>
+   <frame tileid="62" duration="100"/>
+   <frame tileid="86" duration="100"/>
+   <frame tileid="110" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="39" duration="100"/>
+   <frame tileid="63" duration="100"/>
+   <frame tileid="87" duration="100"/>
+   <frame tileid="111" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="40" duration="100"/>
+   <frame tileid="64" duration="100"/>
+   <frame tileid="88" duration="100"/>
+   <frame tileid="112" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="41" duration="100"/>
+   <frame tileid="65" duration="100"/>
+   <frame tileid="89" duration="100"/>
+   <frame tileid="113" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="42" duration="100"/>
+   <frame tileid="66" duration="100"/>
+   <frame tileid="90" duration="100"/>
+   <frame tileid="114" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="19">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="43" duration="100"/>
+   <frame tileid="67" duration="100"/>
+   <frame tileid="91" duration="100"/>
+   <frame tileid="115" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="20">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="44" duration="100"/>
+   <frame tileid="68" duration="100"/>
+   <frame tileid="92" duration="100"/>
+   <frame tileid="116" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="21">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="45" duration="100"/>
+   <frame tileid="69" duration="100"/>
+   <frame tileid="93" duration="100"/>
+   <frame tileid="117" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="22">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="46" duration="100"/>
+   <frame tileid="70" duration="100"/>
+   <frame tileid="94" duration="100"/>
+   <frame tileid="118" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="23">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="37" width="10" height="10">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="47" duration="100"/>
+   <frame tileid="71" duration="100"/>
+   <frame tileid="95" duration="100"/>
+   <frame tileid="119" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
