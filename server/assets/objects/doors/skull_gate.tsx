63f944c131bcc62f50a84be8ef7dbe832b72db46
Realms×Myths <10644454+RealmsCrossMyths@users.noreply.github.com>
2023-09-27 17:35:14 -0400

    upload latest changes

--- server/assets/objects/doors/skull_gate.tsx
+++ server/assets/objects/doors/skull_gate.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.0" name="Door - Skull Gate" tilewidth="34" tileheight="52" tilecount="5" columns="5" objectalignment="bottom">
- <tileoffset x="5" y="5"/>
+ <tileoffset x="0" y="9"/>
  <grid orientation="isometric" width="64" height="32"/>
  <properties>
   <property name="Solid" type="bool" value="true"/>
@@ -8,13 +8,13 @@
  <image source="skull_gate.png" width="170" height="52"/>
  <tile id="0">
   <objectgroup draworder="index" id="3">
-   <object id="3" x="0.127675" y="34.7597" width="38.1138" height="4.40695"/>
+   <object id="3" x="-3" y="24" width="38" height="16"/>
   </objectgroup>
   <animation>
    <frame tileid="0" duration="50"/>
    <frame tileid="1" duration="50"/>
    <frame tileid="2" duration="50"/>
-   <frame tileid="3" duration="50"/>
+   <frame tileid="1" duration="50"/>
   </animation>
  </tile>
 </tileset>
