3ed67155e93989320a92a0471b9b8c800fca80c8
Realms×Myths <10644454+RealmsCrossMyths@users.noreply.github.com>
2023-09-29 10:43:15 -0400

    upload latest changes

--- server/assets/tiles/floors/undernet/gobby_standard.tsx
+++ server/assets/tiles/floors/undernet/gobby_standard.tsx
@@ -2,5 +2,178 @@
 <tileset version="1.8" tiledversion="1.8.0" name="Floors - Undernet - Standard (Gobby)" tilewidth="64" tileheight="40" tilecount="48" columns="4" objectalignment="bottom">
  <tileoffset x="0" y="8"/>
  <grid orientation="isometric" width="64" height="32"/>
- <image source="gobby_standard.png" width="256" height="160"/>
+ <image source="gobby_standard.png" width="256" height="480"/>
+ <tile id="4">
+  <animation>
+   <frame tileid="19" duration="5000"/>
+   <frame tileid="16" duration="20"/>
+   <frame tileid="19" duration="20"/>
+   <frame tileid="16" duration="20"/>
+   <frame tileid="19" duration="20"/>
+   <frame tileid="16" duration="20"/>
+   <frame tileid="19" duration="3000"/>
+   <frame tileid="16" duration="20"/>
+   <frame tileid="19" duration="20"/>
+   <frame tileid="16" duration="20"/>
+   <frame tileid="19" duration="20"/>
+   <frame tileid="16" duration="20"/>
+   <frame tileid="19" duration="20"/>
+   <frame tileid="16" duration="20"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <animation>
+   <frame tileid="28" duration="2000"/>
+   <frame tileid="31" duration="20"/>
+   <frame tileid="28" duration="20"/>
+   <frame tileid="31" duration="20"/>
+   <frame tileid="28" duration="20"/>
+   <frame tileid="31" duration="20"/>
+   <frame tileid="28" duration="5000"/>
+   <frame tileid="31" duration="20"/>
+   <frame tileid="28" duration="20"/>
+   <frame tileid="31" duration="20"/>
+   <frame tileid="28" duration="20"/>
+   <frame tileid="31" duration="20"/>
+   <frame tileid="28" duration="20"/>
+   <frame tileid="31" duration="20"/>
+   <frame tileid="28" duration="20"/>
+   <frame tileid="31" duration="20"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <animation>
+   <frame tileid="40" duration="20"/>
+   <frame tileid="43" duration="20"/>
+   <frame tileid="40" duration="20"/>
+   <frame tileid="43" duration="20"/>
+   <frame tileid="40" duration="20"/>
+   <frame tileid="43" duration="20"/>
+   <frame tileid="40" duration="20"/>
+   <frame tileid="43" duration="3000"/>
+   <frame tileid="40" duration="20"/>
+   <frame tileid="43" duration="20"/>
+   <frame tileid="40" duration="20"/>
+   <frame tileid="43" duration="20"/>
+   <frame tileid="40" duration="1000"/>
+   <frame tileid="43" duration="20"/>
+   <frame tileid="40" duration="20"/>
+   <frame tileid="43" duration="20"/>
+   <frame tileid="40" duration="20"/>
+   <frame tileid="43" duration="3000"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <animation>
+   <frame tileid="43" duration="3000"/>
+   <frame tileid="40" duration="20"/>
+   <frame tileid="43" duration="20"/>
+   <frame tileid="40" duration="20"/>
+   <frame tileid="43" duration="20"/>
+   <frame tileid="40" duration="20"/>
+   <frame tileid="43" duration="20"/>
+   <frame tileid="40" duration="3000"/>
+   <frame tileid="43" duration="20"/>
+   <frame tileid="40" duration="20"/>
+   <frame tileid="43" duration="20"/>
+   <frame tileid="40" duration="20"/>
+   <frame tileid="43" duration="20"/>
+   <frame tileid="40" duration="20"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="23" duration="7000"/>
+   <frame tileid="20" duration="20"/>
+   <frame tileid="23" duration="20"/>
+   <frame tileid="20" duration="20"/>
+   <frame tileid="23" duration="20"/>
+   <frame tileid="20" duration="800"/>
+   <frame tileid="23" duration="20"/>
+   <frame tileid="20" duration="20"/>
+   <frame tileid="23" duration="2000"/>
+   <frame tileid="20" duration="20"/>
+   <frame tileid="23" duration="20"/>
+   <frame tileid="20" duration="20"/>
+   <frame tileid="23" duration="20"/>
+   <frame tileid="20" duration="20"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <animation>
+   <frame tileid="35" duration="5000"/>
+   <frame tileid="32" duration="20"/>
+   <frame tileid="35" duration="20"/>
+   <frame tileid="32" duration="20"/>
+   <frame tileid="35" duration="3000"/>
+   <frame tileid="32" duration="8000"/>
+   <frame tileid="35" duration="20"/>
+   <frame tileid="32" duration="20"/>
+   <frame tileid="35" duration="20"/>
+   <frame tileid="32" duration="20"/>
+   <frame tileid="35" duration="5000"/>
+   <frame tileid="32" duration="20"/>
+   <frame tileid="35" duration="20"/>
+   <frame tileid="32" duration="20"/>
+   <frame tileid="35" duration="20"/>
+   <frame tileid="32" duration="20"/>
+  </animation>
+ </tile>
+ <tile id="10">
+  <animation>
+   <frame tileid="32" duration="4000"/>
+   <frame tileid="35" duration="20"/>
+   <frame tileid="32" duration="20"/>
+   <frame tileid="35" duration="20"/>
+   <frame tileid="32" duration="20"/>
+   <frame tileid="35" duration="20"/>
+   <frame tileid="32" duration="200"/>
+   <frame tileid="35" duration="20"/>
+   <frame tileid="32" duration="20"/>
+   <frame tileid="35" duration="20"/>
+   <frame tileid="32" duration="5000"/>
+   <frame tileid="35" duration="20"/>
+   <frame tileid="32" duration="20"/>
+   <frame tileid="35" duration="20"/>
+   <frame tileid="32" duration="20"/>
+   <frame tileid="35" duration="20"/>
+   <frame tileid="32" duration="4000"/>
+   <frame tileid="35" duration="20"/>
+   <frame tileid="32" duration="20"/>
+   <frame tileid="35" duration="20"/>
+   <frame tileid="32" duration="20"/>
+   <frame tileid="35" duration="20"/>
+   <frame tileid="32" duration="20"/>
+  </animation>
+ </tile>
+ <tile id="11">
+  <animation>
+   <frame tileid="44" duration="3000"/>
+   <frame tileid="47" duration="20"/>
+   <frame tileid="44" duration="20"/>
+   <frame tileid="47" duration="20"/>
+   <frame tileid="44" duration="20"/>
+   <frame tileid="47" duration="20"/>
+   <frame tileid="44" duration="20"/>
+   <frame tileid="47" duration="20"/>
+   <frame tileid="44" duration="20"/>
+   <frame tileid="47" duration="20"/>
+   <frame tileid="44" duration="20"/>
+   <frame tileid="47" duration="7000"/>
+   <frame tileid="44" duration="20"/>
+   <frame tileid="47" duration="20"/>
+   <frame tileid="44" duration="20"/>
+   <frame tileid="47" duration="20"/>
+   <frame tileid="44" duration="20"/>
+   <frame tileid="47" duration="5000"/>
+   <frame tileid="44" duration="20"/>
+   <frame tileid="47" duration="20"/>
+   <frame tileid="44" duration="20"/>
+   <frame tileid="47" duration="20"/>
+   <frame tileid="44" duration="20"/>
+   <frame tileid="47" duration="20"/>
+   <frame tileid="44" duration="20"/>
+   <frame tileid="47" duration="20"/>
+  </animation>
+ </tile>
 </tileset>
