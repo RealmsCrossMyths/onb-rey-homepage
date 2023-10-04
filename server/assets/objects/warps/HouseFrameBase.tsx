faacc27856764ebfea34659bded37a202f6f8576
Realms×Myths <10644454+RealmsCrossMyths@users.noreply.github.com>
2023-10-04 13:15:50 -0400

    upload latest changes

--- server/assets/objects/warps/HouseFrameBase.tsx
+++ server/assets/objects/warps/HouseFrameBase.tsx
@@ -1,38 +1,218 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="House Frame - Base" tilewidth="64" tileheight="32" tilecount="5" columns="5" objectalignment="top">
- <image source="HouseFrameBase.png" width="320" height="32"/>
- <tile id="0">
-  <objectgroup draworder="index" id="2">
-   <object id="2" x="32" y="24">
-    <polygon points="-2,3 -2,8 -30,-6 -30,-11"/>
+<tileset version="1.8" tiledversion="1.8.0" name="House Frame - Base" tilewidth="28" tileheight="17" tilecount="5" columns="5" objectalignment="top">
+ <tileoffset x="0" y="6"/>
+ <image source="HouseFrameBase.png" width="140" height="17"/>
+ <tile id="0" type="Warp">
+  <properties>
+   <property name="Activation Radius" type="float" value="0"/>
+   <property name="Activation Type" value="Radial"/>
+   <property name="Active" type="bool" value="true"/>
+   <property name="Address" value=""/>
+   <property name="Arrival Animation" value=""/>
+   <property name="Direction" value=""/>
+   <property name="Incoming Data" value=""/>
+   <property name="Leave Animation" value=""/>
+   <property name="Offline Check" type="bool" value="false"/>
+   <property name="Offline Tile ID" type="int" value="0"/>
+   <property name="Outgoing Data" value=""/>
+   <property name="Port" type="int" value="0"/>
+   <property name="Server Warp" type="bool" value="false"/>
+   <property name="Target Area" value=""/>
+   <property name="Target Object" type="int" value="0"/>
+  </properties>
+  <objectgroup draworder="index" id="1">
+   <object id="1" type="Launching Collision" x="30" y="10">
+    <properties>
+     <property name="Allow Anim Override" type="bool" value="true"/>
+     <property name="Leave Animation" value="relative_arrow_out"/>
+    </properties>
+    <polygon points="-2,-7 -2,-10 -30,4 -30,7"/>
+   </object>
+   <object id="2" type="Launching Point" x="-18" y="-6">
+    <properties>
+     <property name="Activation Radius" type="float" value="1.15"/>
+     <property name="Allow Anim Override" type="bool" value="true"/>
+     <property name="Allow Radius Override" type="bool" value="false"/>
+     <property name="Leave Animation" value="relative_arrow_out"/>
+    </properties>
+    <point/>
+   </object>
+   <object id="3" type="Landing Point" x="22" y="14">
+    <properties>
+     <property name="Allow Anim Override" type="bool" value="true"/>
+     <property name="Allow Dir Override" type="bool" value="true"/>
+     <property name="Arrival Animation" value="relative_arrow_in"/>
+     <property name="Direction" value="relative_facing"/>
+    </properties>
+    <point/>
    </object>
   </objectgroup>
  </tile>
- <tile id="1">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="32" y="24">
-    <polygon points="-2,4 -2,7 -30,-7 -30,-10"/>
+ <tile id="1" type="Warp">
+  <properties>
+   <property name="Activation Radius" type="float" value="0"/>
+   <property name="Activation Type" value="Radial"/>
+   <property name="Active" type="bool" value="true"/>
+   <property name="Address" value=""/>
+   <property name="Arrival Animation" value=""/>
+   <property name="Direction" value=""/>
+   <property name="Incoming Data" value=""/>
+   <property name="Leave Animation" value=""/>
+   <property name="Offline Check" type="bool" value="false"/>
+   <property name="Offline Tile ID" type="int" value="0"/>
+   <property name="Outgoing Data" value=""/>
+   <property name="Port" type="int" value="0"/>
+   <property name="Server Warp" type="bool" value="false"/>
+   <property name="Target Area" value=""/>
+   <property name="Target Object" type="int" value="0"/>
+  </properties>
+  <objectgroup draworder="index" id="1">
+   <object id="1" type="Launching Collision" x="30" y="10">
+    <polygon points="-2,-7 -2,-10 -30,4 -30,7"/>
+   </object>
+   <object id="2" type="Launching Point" x="-18" y="-6">
+    <properties>
+     <property name="Activation Radius" type="float" value="1.15"/>
+     <property name="Allow Anim Override" type="bool" value="true"/>
+     <property name="Allow Radius Override" type="bool" value="false"/>
+     <property name="Leave Animation" value="relative_arrow_out"/>
+    </properties>
+    <point/>
+   </object>
+   <object id="3" type="Landing Point" x="22" y="14">
+    <properties>
+     <property name="Allow Anim Override" type="bool" value="true"/>
+     <property name="Allow Dir Override" type="bool" value="true"/>
+     <property name="Arrival Animation" value="relative_arrow_in"/>
+     <property name="Direction" value="relative_facing"/>
+    </properties>
+    <point/>
    </object>
   </objectgroup>
  </tile>
- <tile id="2">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="32" y="24">
-    <polygon points="-2,4 -2,7 -30,-7 -30,-10"/>
+ <tile id="2" type="Warp">
+  <properties>
+   <property name="Activation Radius" type="float" value="0"/>
+   <property name="Activation Type" value="Radial"/>
+   <property name="Active" type="bool" value="true"/>
+   <property name="Address" value=""/>
+   <property name="Arrival Animation" value=""/>
+   <property name="Direction" value=""/>
+   <property name="Incoming Data" value=""/>
+   <property name="Leave Animation" value=""/>
+   <property name="Offline Check" type="bool" value="false"/>
+   <property name="Offline Tile ID" type="int" value="0"/>
+   <property name="Outgoing Data" value=""/>
+   <property name="Port" type="int" value="0"/>
+   <property name="Server Warp" type="bool" value="false"/>
+   <property name="Target Area" value=""/>
+   <property name="Target Object" type="int" value="0"/>
+  </properties>
+  <objectgroup draworder="index" id="1">
+   <object id="1" type="Launching Collision" x="30" y="10">
+    <polygon points="-2,-7 -2,-10 -30,4 -30,7"/>
+   </object>
+   <object id="2" type="Launching Point" x="-18" y="-6">
+    <properties>
+     <property name="Activation Radius" type="float" value="1.15"/>
+     <property name="Allow Anim Override" type="bool" value="true"/>
+     <property name="Allow Radius Override" type="bool" value="false"/>
+     <property name="Leave Animation" value="relative_arrow_out"/>
+    </properties>
+    <point/>
+   </object>
+   <object id="3" type="Landing Point" x="22" y="14">
+    <properties>
+     <property name="Allow Anim Override" type="bool" value="true"/>
+     <property name="Allow Dir Override" type="bool" value="true"/>
+     <property name="Arrival Animation" value="relative_arrow_in"/>
+     <property name="Direction" value="relative_facing"/>
+    </properties>
+    <point/>
    </object>
   </objectgroup>
  </tile>
- <tile id="3">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="32" y="24">
-    <polygon points="-2,4 -2,7 -30,-7 -30,-10"/>
+ <tile id="3" type="Warp">
+  <properties>
+   <property name="Activation Radius" type="float" value="0"/>
+   <property name="Activation Type" value="Radial"/>
+   <property name="Active" type="bool" value="true"/>
+   <property name="Address" value=""/>
+   <property name="Arrival Animation" value=""/>
+   <property name="Direction" value=""/>
+   <property name="Incoming Data" value=""/>
+   <property name="Leave Animation" value=""/>
+   <property name="Offline Check" type="bool" value="false"/>
+   <property name="Offline Tile ID" type="int" value="0"/>
+   <property name="Outgoing Data" value=""/>
+   <property name="Port" type="int" value="0"/>
+   <property name="Server Warp" type="bool" value="false"/>
+   <property name="Target Area" value=""/>
+   <property name="Target Object" type="int" value="0"/>
+  </properties>
+  <objectgroup draworder="index" id="1">
+   <object id="1" type="Launching Collision" x="30" y="10">
+    <polygon points="-2,-7 -2,-10 -30,4 -30,7"/>
+   </object>
+   <object id="2" type="Launching Point" x="-18" y="-6">
+    <properties>
+     <property name="Activation Radius" type="float" value="1.15"/>
+     <property name="Allow Anim Override" type="bool" value="true"/>
+     <property name="Allow Radius Override" type="bool" value="false"/>
+     <property name="Leave Animation" value="relative_arrow_out"/>
+    </properties>
+    <point/>
+   </object>
+   <object id="3" type="Landing Point" x="22" y="14">
+    <properties>
+     <property name="Allow Anim Override" type="bool" value="true"/>
+     <property name="Allow Dir Override" type="bool" value="true"/>
+     <property name="Arrival Animation" value="relative_arrow_in"/>
+     <property name="Direction" value="relative_facing"/>
+    </properties>
+    <point/>
    </object>
   </objectgroup>
  </tile>
- <tile id="4">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="32" y="24">
-    <polygon points="-2,4 -2,7 -30,-7 -30,-10"/>
+ <tile id="4" type="Warp">
+  <properties>
+   <property name="Activation Radius" type="float" value="0"/>
+   <property name="Activation Type" value="Radial"/>
+   <property name="Active" type="bool" value="true"/>
+   <property name="Address" value=""/>
+   <property name="Arrival Animation" value=""/>
+   <property name="Direction" value=""/>
+   <property name="Incoming Data" value=""/>
+   <property name="Leave Animation" value=""/>
+   <property name="Offline Check" type="bool" value="false"/>
+   <property name="Offline Tile ID" type="int" value="0"/>
+   <property name="Outgoing Data" value=""/>
+   <property name="Port" type="int" value="0"/>
+   <property name="Server Warp" type="bool" value="false"/>
+   <property name="Target Area" value=""/>
+   <property name="Target Object" type="int" value="0"/>
+  </properties>
+  <objectgroup draworder="index" id="1">
+   <object id="1" type="Launching Collision" x="30" y="10">
+    <polygon points="-2,-7 -2,-10 -30,4 -30,7"/>
+   </object>
+   <object id="2" type="Launching Point" x="-18" y="-6">
+    <properties>
+     <property name="Activation Radius" type="float" value="1.15"/>
+     <property name="Allow Anim Override" type="bool" value="true"/>
+     <property name="Allow Radius Override" type="bool" value="false"/>
+     <property name="Leave Animation" value="relative_arrow_out"/>
+    </properties>
+    <point/>
+   </object>
+   <object id="3" type="Landing Point" x="22" y="14">
+    <properties>
+     <property name="Allow Anim Override" type="bool" value="true"/>
+     <property name="Allow Dir Override" type="bool" value="true"/>
+     <property name="Arrival Animation" value="relative_arrow_in"/>
+     <property name="Direction" value="relative_facing"/>
+    </properties>
+    <point/>
    </object>
   </objectgroup>
  </tile>
