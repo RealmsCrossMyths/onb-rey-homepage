f9823b9abbf4f04a9cba5f02ad673826eeb0e232
Realms×Myths <10644454+RealmsCrossMyths@users.noreply.github.com>
2023-10-04 20:41:38 -0400

    upload latest changes

---
+++ server/assets/objects/signs/cyber_noticeboard.tsx
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="Signs - Notice Board" tilewidth="48" tileheight="68" tilecount="20" columns="5" objectalignment="bottom">
+ <tileoffset x="0" y="15"/>
+ <image source="cyber_noticeboard.png" width="240" height="272"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6" y="69">
+    <polygon points="-6,8 -6,-16 42,-40 42,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6" y="69">
+    <polygon points="-6,8 -6,-16 42,-40 42,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6" y="69">
+    <polygon points="-6,8 -6,-16 42,-40 42,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="6" y="69">
+    <polygon points="-6,8 -6,-16 42,-40 42,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
