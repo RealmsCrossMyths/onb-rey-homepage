6e803f8860aad2a08b666c843b8cee920716fbfc
Realms×Myths <10644454+RealmsCrossMyths@users.noreply.github.com>
2023-10-09 14:52:50 -0400

    upload latest changes

--- server/assets/objects/walls/invisible.tsx
+++ server/assets/objects/walls/invisible.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="Wall - Invisible" tilewidth="64" tileheight="32" tilecount="1" columns="1" objectalignment="center">
- <image source="../../miscellaneous/empty.png" width="64" height="32"/>
+<tileset version="1.8" tiledversion="1.8.0" name="Wall - Invisible" tilewidth="34" tileheight="17" tilecount="1" columns="1" objectalignment="center">
+ <image source="invisible.png" width="34" height="17"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="16">
