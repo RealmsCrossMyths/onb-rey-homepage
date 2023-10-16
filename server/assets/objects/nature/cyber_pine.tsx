80c902ffaf1402e28d16dbbbc36e5f8a547760e7
Realms×Myths <10644454+RealmsCrossMyths@users.noreply.github.com>
2023-10-16 13:31:53 -0400

    upload latest changes

--- server/assets/objects/nature/cyber_pine.tsx
+++ server/assets/objects/nature/cyber_pine.tsx
@@ -1,14 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="Nature - Cybernet Pine" tilewidth="64" tileheight="67" tilecount="4" columns="4" objectalignment="bottom">
- <tileoffset x="0" y="16"/>
+<tileset version="1.8" tiledversion="1.8.0" name="Nature - Cybernet Pine" tilewidth="42" tileheight="61" tilecount="4" columns="4" objectalignment="bottom">
+ <tileoffset x="0" y="11"/>
  <grid orientation="orthogonal" width="64" height="32"/>
  <properties>
   <property name="Solid" type="bool" value="true"/>
  </properties>
- <image source="cyber_pine.png" width="256" height="67"/>
+ <image source="cyber_pine.png" width="168" height="61"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="12" y="40" width="40" height="21">
+   <object id="1" x="0" y="39" width="42" height="22">
     <ellipse/>
    </object>
   </objectgroup>
