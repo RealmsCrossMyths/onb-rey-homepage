8fd88493d9d1d58970d3fbc27dd22836eef95afd
Realms×Myths <10644454+RealmsCrossMyths@users.noreply.github.com>
2023-10-15 21:14:42 -0400

    upload latest changes

---
+++ server/assets/tiles/arrows/cybernet/standard-00-forest.tsx
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="Exit Arrows — Cybernet — Standard" tilewidth="110" tileheight="72" tilecount="48" columns="4" objectalignment="top">
+ <tileoffset x="-23" y="20"/>
+ <grid orientation="isometric" width="64" height="32"/>
+ <image source="standard-00-forest.png" width="440" height="864"/>
+</tileset>
--- server/assets/tiles/floors/undernet/standard-01-denim.tsx
+++ server/assets/tiles/floors/undernet/standard-01-denim.tsx
@@ -1,6 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="Floors — Undernet — Standard #1 — Denim" tilewidth="64" tileheight="40" tilecount="144" columns="12" objectalignment="top">
+<tileset version="1.8" tiledversion="1.8.0" name="Floors — Undernet — Standard #1 — Denim" tilewidth="64" tileheight="40" tilecount="96" columns="8">
  <tileoffset x="0" y="8"/>
- <grid orientation="isometric" width="64" height="32"/>
- <image source="standard-01-denim.png" width="768" height="480"/>
+ <image source="standard-01-denim.png" width="512" height="480"/>
 </tileset>
