5346c2ebd9c089a1c4c401165f063d4317e2b903
Realms×Myths <10644454+RealmsCrossMyths@users.noreply.github.com>
2023-10-17 17:37:19 -0400

    upload latest changes

---
+++ server/assets/tiles/floors/special/glass.tsx
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="Floors - Special - Glass" tilewidth="64" tileheight="40" tilecount="4" columns="2" objectalignment="top">
+ <tileoffset x="0" y="8"/>
+ <grid orientation="isometric" width="64" height="32"/>
+ <image source="glass.png" width="128" height="80"/>
+</tileset>
---
+++ server/assets/tiles/stairs/gobby-sampler2.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="Stairs - Gobby Sampler" tilewidth="64" tileheight="57" tilecount="6" columns="3" objectalignment="bottom">
+ <tileoffset x="0" y="9"/>
+ <image source="gobby-sampler2.png" width="192" height="114"/>
+</tileset>
