0c085f48a32fd9c87582618378df876a15546a1f
Realms×Myths <10644454+RealmsCrossMyths@users.noreply.github.com>
2023-10-17 20:04:48 -0400

    upload latest changes

--- server/assets/tiles/stairs/bullpoopstairscanfrackthemselves.tsx
+++ server/assets/tiles/stairs/bullpoopstairscanfrackthemselves.tsx
@@ -1,6 +1,7 @@
-<tileset version="1.8" tiledversion="1.8.0" name="Stairs - Gobby Sampler" tilewidth="64" tileheight="57" tilecount="6" columns="3" objectalignment="bottom">
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="bullpoopstairscanfrackthemselves" tilewidth="64" tileheight="57" tilecount="6" columns="3">
  <tileoffset x="0" y="9"/>
- <image source="gobby-sampler.png" width="192" height="114"/>
+ <image source="bullpoopstairscanfrackthemselves.png" width="192" height="114"/>
  <tile id="0" type="Stairs">
   <properties>
    <property name="Direction" value="Up Left"/>
@@ -11,22 +12,24 @@
    <property name="Direction" value="Up Left"/>
   </properties>
  </tile>
- <tile id="2">
+ <tile id="2" type="Stairs">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="16" width="58" height="35"/>
+   <object id="1" x="0" y="17" width="58" height="35"/>
   </objectgroup>
  </tile>
  <tile id="3" type="Stairs">
   <properties>
    <property name="Direction" value="Down Right"/>
   </properties>
+ </tile>
  <tile id="4" type="Stairs">
   <properties>
    <property name="Direction" value="Down Right"/>
   </properties>
- <tile id="5">
+ </tile>
+ <tile id="5" type="Stairs">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="16" width="58" height="35"/>
+   <object id="1" x="0" y="32" width="64" height="35"/>
   </objectgroup>
  </tile>
 </tileset>
