7e5d85af46ac5ba7322227516b1740f7acdee5d6
Realms×Myths <10644454+RealmsCrossMyths@users.noreply.github.com>
2023-10-21 17:37:07 -0400

    upload latest changes

---
+++ server/assets/tiles/floors/cybernet/standard-00-1-forest.tsx
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="Floors — Cybernet — Standard #0 — Forest" tilewidth="64" tileheight="40" tilecount="144" columns="12" objectalignment="top">
+ <tileoffset x="0" y="8"/>
+ <grid orientation="isometric" width="64" height="32"/>
+ <image source="standard-00-1-forest.png" width="768" height="480"/>
+</tileset>
