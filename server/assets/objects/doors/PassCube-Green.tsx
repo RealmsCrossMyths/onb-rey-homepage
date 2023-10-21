45de91a6a45c8868cdf3f86fb2887334589e147f
Realms×Myths <10644454+RealmsCrossMyths@users.noreply.github.com>
2023-10-20 21:15:08 -0400

    upload latest changes

---
+++ server/assets/objects/doors/PassCube-Green.tsx
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="PassCube — Green" tilewidth="42" tileheight="44" tilecount="4" columns="4">
+ <tileoffset x="0" y="11"/>
+ <image source="../../ezlibs-assets/ezcheckpoints/bn5cubegreen_tile.png" width="168" height="44"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="-6" y="14">
+    <polygon points="-5,19 27,3 59,19 27,35"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="133"/>
+   <frame tileid="3" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-6" y="14">
+    <polygon points="-5,19 27,3 59,19 27,35"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-6" y="14">
+    <polygon points="-5,19 27,3 59,19 27,35"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-6" y="14">
+    <polygon points="-5,19 27,3 59,19 27,35"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
