8324f331c11794dffee02cef809a4138845f9d47
Realms×Myths <10644454+RealmsCrossMyths@users.noreply.github.com>
2023-10-26 20:38:40 -0400

    upload latest changes

---
+++ server/assets/objects/decor/warps/HouseFrameBase.tsx
@@ -0,0 +1,219 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="House Frame - Base" tilewidth="28" tileheight="17" tilecount="5" columns="5" objectalignment="top">
+ <tileoffset x="0" y="6"/>
+ <image source="HouseFrameBase.png" width="140" height="17"/>
+ <tile id="0" type="Warp">
+  <properties>
+   <property name="Activation Radius" type="float" value="0"/>
+   <property name="Activation Type" value="Radial"/>
+   <property name="Active" type="bool" value="true"/>
+   <property name="Address" value=""/>
+   <property name="Arrival Animation" value=""/>
+   <property name="Direction" value=""/>
+   <property name="Incoming Data" value=""/>
+   <property name="Leave Animation" value=""/>
+   <property name="Offline Check" type="bool" value="false"/>
+   <property name="Offline Tile ID" type="int" value="0"/>
+   <property name="Outgoing Data" value=""/>
+   <property name="Port" type="int" value="0"/>
+   <property name="Server Warp" type="bool" value="false"/>
+   <property name="Target Area" value=""/>
+   <property name="Target Object" type="int" value="0"/>
+  </properties>
+  <objectgroup draworder="index" id="1">
+   <object id="1" type="Launching Collision" x="30" y="10">
+    <properties>
+     <property name="Allow Anim Override" type="bool" value="true"/>
+     <property name="Leave Animation" value="relative_arrow_out"/>
+    </properties>
+    <polygon points="-2,-7 -2,-10 -30,4 -30,7"/>
+   </object>
+   <object id="2" type="Launching Point" x="-18" y="-6">
+    <properties>
+     <property name="Activation Radius" type="float" value="1.15"/>
+     <property name="Allow Anim Override" type="bool" value="true"/>
+     <property name="Allow Radius Override" type="bool" value="false"/>
+     <property name="Leave Animation" value="relative_arrow_out"/>
+    </properties>
+    <point/>
+   </object>
+   <object id="3" type="Landing Point" x="22" y="14">
+    <properties>
+     <property name="Allow Anim Override" type="bool" value="true"/>
+     <property name="Allow Dir Override" type="bool" value="true"/>
+     <property name="Arrival Animation" value="relative_arrow_in"/>
+     <property name="Direction" value="relative_facing"/>
+    </properties>
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1" type="Warp">
+  <properties>
+   <property name="Activation Radius" type="float" value="0"/>
+   <property name="Activation Type" value="Radial"/>
+   <property name="Active" type="bool" value="true"/>
+   <property name="Address" value=""/>
+   <property name="Arrival Animation" value=""/>
+   <property name="Direction" value=""/>
+   <property name="Incoming Data" value=""/>
+   <property name="Leave Animation" value=""/>
+   <property name="Offline Check" type="bool" value="false"/>
+   <property name="Offline Tile ID" type="int" value="0"/>
+   <property name="Outgoing Data" value=""/>
+   <property name="Port" type="int" value="0"/>
+   <property name="Server Warp" type="bool" value="false"/>
+   <property name="Target Area" value=""/>
+   <property name="Target Object" type="int" value="0"/>
+  </properties>
+  <objectgroup draworder="index" id="1">
+   <object id="1" type="Launching Collision" x="30" y="10">
+    <polygon points="-2,-7 -2,-10 -30,4 -30,7"/>
+   </object>
+   <object id="2" type="Launching Point" x="-18" y="-6">
+    <properties>
+     <property name="Activation Radius" type="float" value="1.15"/>
+     <property name="Allow Anim Override" type="bool" value="true"/>
+     <property name="Allow Radius Override" type="bool" value="false"/>
+     <property name="Leave Animation" value="relative_arrow_out"/>
+    </properties>
+    <point/>
+   </object>
+   <object id="3" type="Landing Point" x="22" y="14">
+    <properties>
+     <property name="Allow Anim Override" type="bool" value="true"/>
+     <property name="Allow Dir Override" type="bool" value="true"/>
+     <property name="Arrival Animation" value="relative_arrow_in"/>
+     <property name="Direction" value="relative_facing"/>
+    </properties>
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2" type="Warp">
+  <properties>
+   <property name="Activation Radius" type="float" value="0"/>
+   <property name="Activation Type" value="Radial"/>
+   <property name="Active" type="bool" value="true"/>
+   <property name="Address" value=""/>
+   <property name="Arrival Animation" value=""/>
+   <property name="Direction" value=""/>
+   <property name="Incoming Data" value=""/>
+   <property name="Leave Animation" value=""/>
+   <property name="Offline Check" type="bool" value="false"/>
+   <property name="Offline Tile ID" type="int" value="0"/>
+   <property name="Outgoing Data" value=""/>
+   <property name="Port" type="int" value="0"/>
+   <property name="Server Warp" type="bool" value="false"/>
+   <property name="Target Area" value=""/>
+   <property name="Target Object" type="int" value="0"/>
+  </properties>
+  <objectgroup draworder="index" id="1">
+   <object id="1" type="Launching Collision" x="30" y="10">
+    <polygon points="-2,-7 -2,-10 -30,4 -30,7"/>
+   </object>
+   <object id="2" type="Launching Point" x="-18" y="-6">
+    <properties>
+     <property name="Activation Radius" type="float" value="1.15"/>
+     <property name="Allow Anim Override" type="bool" value="true"/>
+     <property name="Allow Radius Override" type="bool" value="false"/>
+     <property name="Leave Animation" value="relative_arrow_out"/>
+    </properties>
+    <point/>
+   </object>
+   <object id="3" type="Landing Point" x="22" y="14">
+    <properties>
+     <property name="Allow Anim Override" type="bool" value="true"/>
+     <property name="Allow Dir Override" type="bool" value="true"/>
+     <property name="Arrival Animation" value="relative_arrow_in"/>
+     <property name="Direction" value="relative_facing"/>
+    </properties>
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3" type="Warp">
+  <properties>
+   <property name="Activation Radius" type="float" value="0"/>
+   <property name="Activation Type" value="Radial"/>
+   <property name="Active" type="bool" value="true"/>
+   <property name="Address" value=""/>
+   <property name="Arrival Animation" value=""/>
+   <property name="Direction" value=""/>
+   <property name="Incoming Data" value=""/>
+   <property name="Leave Animation" value=""/>
+   <property name="Offline Check" type="bool" value="false"/>
+   <property name="Offline Tile ID" type="int" value="0"/>
+   <property name="Outgoing Data" value=""/>
+   <property name="Port" type="int" value="0"/>
+   <property name="Server Warp" type="bool" value="false"/>
+   <property name="Target Area" value=""/>
+   <property name="Target Object" type="int" value="0"/>
+  </properties>
+  <objectgroup draworder="index" id="1">
+   <object id="1" type="Launching Collision" x="30" y="10">
+    <polygon points="-2,-7 -2,-10 -30,4 -30,7"/>
+   </object>
+   <object id="2" type="Launching Point" x="-18" y="-6">
+    <properties>
+     <property name="Activation Radius" type="float" value="1.15"/>
+     <property name="Allow Anim Override" type="bool" value="true"/>
+     <property name="Allow Radius Override" type="bool" value="false"/>
+     <property name="Leave Animation" value="relative_arrow_out"/>
+    </properties>
+    <point/>
+   </object>
+   <object id="3" type="Landing Point" x="22" y="14">
+    <properties>
+     <property name="Allow Anim Override" type="bool" value="true"/>
+     <property name="Allow Dir Override" type="bool" value="true"/>
+     <property name="Arrival Animation" value="relative_arrow_in"/>
+     <property name="Direction" value="relative_facing"/>
+    </properties>
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4" type="Warp">
+  <properties>
+   <property name="Activation Radius" type="float" value="0"/>
+   <property name="Activation Type" value="Radial"/>
+   <property name="Active" type="bool" value="true"/>
+   <property name="Address" value=""/>
+   <property name="Arrival Animation" value=""/>
+   <property name="Direction" value=""/>
+   <property name="Incoming Data" value=""/>
+   <property name="Leave Animation" value=""/>
+   <property name="Offline Check" type="bool" value="false"/>
+   <property name="Offline Tile ID" type="int" value="0"/>
+   <property name="Outgoing Data" value=""/>
+   <property name="Port" type="int" value="0"/>
+   <property name="Server Warp" type="bool" value="false"/>
+   <property name="Target Area" value=""/>
+   <property name="Target Object" type="int" value="0"/>
+  </properties>
+  <objectgroup draworder="index" id="1">
+   <object id="1" type="Launching Collision" x="30" y="10">
+    <polygon points="-2,-7 -2,-10 -30,4 -30,7"/>
+   </object>
+   <object id="2" type="Launching Point" x="-18" y="-6">
+    <properties>
+     <property name="Activation Radius" type="float" value="1.15"/>
+     <property name="Allow Anim Override" type="bool" value="true"/>
+     <property name="Allow Radius Override" type="bool" value="false"/>
+     <property name="Leave Animation" value="relative_arrow_out"/>
+    </properties>
+    <point/>
+   </object>
+   <object id="3" type="Landing Point" x="22" y="14">
+    <properties>
+     <property name="Allow Anim Override" type="bool" value="true"/>
+     <property name="Allow Dir Override" type="bool" value="true"/>
+     <property name="Arrival Animation" value="relative_arrow_in"/>
+     <property name="Direction" value="relative_facing"/>
+    </properties>
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ server/assets/tiles/conveyor/g+f_style.tsx
@@ -0,0 +1,199 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="Conveyors — G+F Style" tilewidth="64" tileheight="40" tilecount="256" columns="16" objectalignment="top">
+ <tileoffset x="0" y="8"/>
+ <grid orientation="isometric" width="64" height="32"/>
+ <image source="g+f_style.png" width="1024" height="640"/>
+ <tile id="0" type="Conveyor">
+  <properties>
+   <property name="Direction" value="Up Left"/>
+   <property name="speed" type="float" value="6"/>
+  </properties>
+  <animation>
+   <frame tileid="0" duration="90"/>
+   <frame tileid="64" duration="90"/>
+   <frame tileid="128" duration="90"/>
+   <frame tileid="192" duration="90"/>
+  </animation>
+ </tile>
+ <tile id="1" type="Conveyor">
+  <properties>
+   <property name="Direction" value="Up Left"/>
+   <property name="speed" type="float" value="6"/>
+  </properties>
+  <animation>
+   <frame tileid="1" duration="90"/>
+   <frame tileid="65" duration="90"/>
+   <frame tileid="129" duration="90"/>
+   <frame tileid="193" duration="90"/>
+  </animation>
+ </tile>
+ <tile id="2" type="Conveyor">
+  <properties>
+   <property name="Direction" value="Up Left"/>
+   <property name="speed" type="float" value="6"/>
+  </properties>
+  <animation>
+   <frame tileid="2" duration="90"/>
+   <frame tileid="66" duration="90"/>
+   <frame tileid="130" duration="90"/>
+   <frame tileid="194" duration="90"/>
+  </animation>
+ </tile>
+ <tile id="3" type="Conveyor">
+  <properties>
+   <property name="Direction" value="Up Left"/>
+   <property name="speed" type="float" value="6"/>
+  </properties>
+  <animation>
+   <frame tileid="3" duration="90"/>
+   <frame tileid="67" duration="90"/>
+   <frame tileid="131" duration="90"/>
+   <frame tileid="195" duration="90"/>
+  </animation>
+ </tile>
+ <tile id="16" type="Conveyor">
+  <properties>
+   <property name="Direction" value="Up Right"/>
+   <property name="speed" type="float" value="6"/>
+  </properties>
+  <animation>
+   <frame tileid="16" duration="90"/>
+   <frame tileid="80" duration="90"/>
+   <frame tileid="144" duration="90"/>
+   <frame tileid="208" duration="90"/>
+  </animation>
+ </tile>
+ <tile id="17" type="Conveyor">
+  <properties>
+   <property name="Direction" value="Up Right"/>
+   <property name="speed" type="float" value="6"/>
+  </properties>
+  <animation>
+   <frame tileid="17" duration="90"/>
+   <frame tileid="81" duration="90"/>
+   <frame tileid="145" duration="90"/>
+   <frame tileid="209" duration="90"/>
+  </animation>
+ </tile>
+ <tile id="18" type="Conveyor">
+  <properties>
+   <property name="Direction" value="Up Right"/>
+   <property name="speed" type="float" value="6"/>
+  </properties>
+  <animation>
+   <frame tileid="18" duration="90"/>
+   <frame tileid="82" duration="90"/>
+   <frame tileid="146" duration="90"/>
+   <frame tileid="210" duration="90"/>
+  </animation>
+ </tile>
+ <tile id="19" type="Conveyor">
+  <properties>
+   <property name="Direction" value="Up Right"/>
+   <property name="speed" type="float" value="6"/>
+  </properties>
+  <animation>
+   <frame tileid="19" duration="90"/>
+   <frame tileid="83" duration="90"/>
+   <frame tileid="147" duration="90"/>
+   <frame tileid="211" duration="90"/>
+  </animation>
+ </tile>
+ <tile id="32" type="Conveyor">
+  <properties>
+   <property name="Direction" value="Down Right"/>
+   <property name="speed" type="float" value="6"/>
+  </properties>
+  <animation>
+   <frame tileid="32" duration="90"/>
+   <frame tileid="96" duration="90"/>
+   <frame tileid="160" duration="90"/>
+   <frame tileid="224" duration="90"/>
+  </animation>
+ </tile>
+ <tile id="33" type="Conveyor">
+  <properties>
+   <property name="Direction" value="Down Right"/>
+   <property name="speed" type="float" value="6"/>
+  </properties>
+  <animation>
+   <frame tileid="33" duration="90"/>
+   <frame tileid="97" duration="90"/>
+   <frame tileid="161" duration="90"/>
+   <frame tileid="225" duration="90"/>
+  </animation>
+ </tile>
+ <tile id="34" type="Conveyor">
+  <properties>
+   <property name="Direction" value="Down Right"/>
+   <property name="speed" type="float" value="6"/>
+  </properties>
+  <animation>
+   <frame tileid="34" duration="90"/>
+   <frame tileid="98" duration="90"/>
+   <frame tileid="162" duration="90"/>
+   <frame tileid="226" duration="90"/>
+  </animation>
+ </tile>
+ <tile id="35" type="Conveyor">
+  <properties>
+   <property name="Direction" value="Down Right"/>
+   <property name="speed" type="float" value="6"/>
+  </properties>
+  <animation>
+   <frame tileid="35" duration="90"/>
+   <frame tileid="99" duration="90"/>
+   <frame tileid="163" duration="90"/>
+   <frame tileid="227" duration="90"/>
+  </animation>
+ </tile>
+ <tile id="48" type="Conveyor">
+  <properties>
+   <property name="Direction" value="Down Left"/>
+   <property name="speed" type="float" value="6"/>
+  </properties>
+  <animation>
+   <frame tileid="48" duration="90"/>
+   <frame tileid="112" duration="90"/>
+   <frame tileid="176" duration="90"/>
+   <frame tileid="240" duration="90"/>
+  </animation>
+ </tile>
+ <tile id="49" type="Conveyor">
+  <properties>
+   <property name="Direction" value="Down Left"/>
+   <property name="speed" type="float" value="6"/>
+  </properties>
+  <animation>
+   <frame tileid="49" duration="90"/>
+   <frame tileid="113" duration="90"/>
+   <frame tileid="177" duration="90"/>
+   <frame tileid="241" duration="90"/>
+  </animation>
+ </tile>
+ <tile id="50" type="Conveyor">
+  <properties>
+   <property name="Direction" value="Down Left"/>
+   <property name="speed" type="float" value="6"/>
+  </properties>
+  <animation>
+   <frame tileid="50" duration="90"/>
+   <frame tileid="114" duration="90"/>
+   <frame tileid="178" duration="90"/>
+   <frame tileid="242" duration="90"/>
+  </animation>
+ </tile>
+ <tile id="51" type="Conveyor">
+  <properties>
+   <property name="Direction" value="Down Left"/>
+   <property name="Sound Effect" value="server/assets/sounds/effects/conveyor.ogg"/>
+   <property name="speed" type="float" value="6"/>
+  </properties>
+  <animation>
+   <frame tileid="51" duration="90"/>
+   <frame tileid="115" duration="90"/>
+   <frame tileid="179" duration="90"/>
+   <frame tileid="243" duration="90"/>
+  </animation>
+ </tile>
+</tileset>
