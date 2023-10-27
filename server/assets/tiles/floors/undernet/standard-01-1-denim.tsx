040ce2aee56cacb2ad3b73491f7240bee743f2d8
Realms×Myths <10644454+RealmsCrossMyths@users.noreply.github.com>
2023-10-26 21:01:40 -0400

    upload latest changes

--- server/assets/tiles/floors/undernet/standard-01-1-denim.tsx
+++ server/assets/tiles/floors/undernet/standard-01-1-denim.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="Floors — Undernet — Standard #1 — Denim" tilewidth="64" tileheight="40" tilecount="96" columns="8">
+<tileset version="1.8" tiledversion="1.8.0" name="Floors — Undernet — Standard #1 — Denim" tilewidth="64" tileheight="40" tilecount="96" columns="8" objectalignment="top">
  <tileoffset x="0" y="8"/>
  <grid orientation="orthogonal" width="64" height="32"/>
  <image source="standard-01-1-denim.png" width="512" height="480"/>
