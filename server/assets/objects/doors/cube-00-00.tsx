60c0af08f8c8a2cf6f5b07b02f323b4efbf591f1
Realms×Myths <10644454+RealmsCrossMyths@users.noreply.github.com>
2024-01-28 09:21:55 -0500

    upload latest changes

--- server/assets/objects/doors/cube-00-00.tsx
+++ server/assets/objects/doors/cube-00-00.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="Security Cube — Gray\Gray" tilewidth="48" tileheight="52" tilecount="10" columns="5" objectalignment="top">
+<tileset version="1.8" tiledversion="1.8.0" name="Security Cube — Gray\Gray" tilewidth="48" tileheight="52" tilecount="10" columns="5" objectalignment="bottomleft">
  <tileoffset x="8" y="-6"/>
  <grid orientation="orthogonal" width="64" height="32"/>
  <image source="cube-00-00.png" width="240" height="104"/>
