db5ddfdf7a039de684465b7917916c039edb00bf
Realms×Myths <10644454+RealmsCrossMyths@users.noreply.github.com>
2024-01-28 08:58:58 -0500

    upload latest changes

---
+++ server/assets/objects/doors/cube-00-00.tsx
@@ -0,0 +1,136 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="Security Cube — Gray\Gray" tilewidth="48" tileheight="52" tilecount="10" columns="5" objectalignment="top">
+ <tileoffset x="8" y="-6"/>
+ <grid orientation="orthogonal" width="64" height="32"/>
+ <image source="cube-00-00.png" width="240" height="104"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="1">
+   <object id="1" x="-3" y="23">
+    <polygon points="-5,19 27,3 59,19 27,35"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="1" duration="133"/>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="2" duration="133"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="1">
+   <object id="1" x="-3" y="23">
+    <polygon points="-5,19 27,3 59,19 27,35"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="1" duration="133"/>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="2" duration="133"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="1">
+   <object id="1" x="-3" y="23">
+    <polygon points="-5,19 27,3 59,19 27,35"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="1" duration="133"/>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="2" duration="133"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="1">
+   <object id="1" x="-3" y="23">
+    <polygon points="-5,19 27,3 59,19 27,35"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="1" duration="133"/>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="2" duration="133"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="1">
+   <object id="1" x="-3" y="23">
+    <polygon points="-5,19 27,3 59,19 27,35"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="1" duration="133"/>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="2" duration="133"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="1">
+   <object id="1" x="-3" y="23">
+    <polygon points="-5,19 27,3 59,19 27,35"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="1" duration="133"/>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="2" duration="133"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="1">
+   <object id="1" x="-3" y="23">
+    <polygon points="-5,19 27,3 59,19 27,35"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="1" duration="133"/>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="2" duration="133"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="1">
+   <object id="1" x="-3" y="23">
+    <polygon points="-5,19 27,3 59,19 27,35"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="1" duration="133"/>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="2" duration="133"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="1">
+   <object id="1" x="-3" y="23">
+    <polygon points="-5,19 27,3 59,19 27,35"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="1" duration="133"/>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="2" duration="133"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="1">
+   <object id="1" x="-3" y="23">
+    <polygon points="-5,19 27,3 59,19 27,35"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="1" duration="133"/>
+   <frame tileid="0" duration="133"/>
+   <frame tileid="2" duration="133"/>
+  </animation>
+ </tile>
+</tileset>
